415903af0466dad0b6b4e4b486527c11366ffd36
Deepanshu <deepanshuarora@Deepanshus-MacBook-Air.local>
2018-05-18 17:57:16 +0530

    fix indentation

--- src/main/java/OwnerTest.java
+++ src/main/java/OwnerTest.java
@@ -2,10 +2,10 @@ class OwnerTest {
 
   public static void main(String[] args) {
 
-  	Wallet wallet = new Wallet();
-  	Owner owner = new Owner();
+    Wallet wallet = new Wallet();
+    Owner owner = new Owner();
 
-  	owner.addMoney(500, wallet);
+    owner.addMoney(500, wallet);
     owner.addMoney(200, wallet);
     owner.buy(1000,wallet);
     owner.buy(20,wallet);
