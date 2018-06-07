10080dcbbb5deeebf180a9777a044a4bdb437065
Deepanshu <Deepanshu@Akshays-MacBook-Pro.local>
2018-06-08 02:44:02 +0530

    refactoring: added setUp method

--- src/test/java/OwnerTest.java
+++ src/test/java/OwnerTest.java
@@ -1,19 +1,23 @@
 import org.junit.Test;
 import static org.junit.Assert.assertEquals;
+import org.junit.Before;
 
 public class OwnerTest {
   private Owner owner;
 
+  @Before
+  public void setUp() {
+    owner = new Owner();
+  }
+
   @Test
   public void checkAddMoney() {
-    owner = new Owner();
     owner.addMoney(400);
     assertEquals(400, owner.getWalletBalance());
   }
 
   @Test
   public void checkBuy() {
-    owner = new Owner();
     owner.addMoney(200);
     owner.buy(100);
     assertEquals(100, owner.getWalletBalance());
@@ -21,7 +25,6 @@ public class OwnerTest {
 
   @Test
   public void checkBuyWhenBalanceInsufficient() {
-    owner = new Owner();
     owner.addMoney(100);
     owner.buy(200);
     assertEquals(100, owner.getWalletBalance());
@@ -29,7 +32,6 @@ public class OwnerTest {
 
   @Test
   public void checkBuyWhenNegativeAmountEnteredByOwner() {
-    owner = new Owner();
     owner.addMoney(100);
     owner.buy(-900);
     assertEquals(100, owner.getWalletBalance());
